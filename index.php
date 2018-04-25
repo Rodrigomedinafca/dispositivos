eaceb5a9374e9a49821e0441ff3e91369fd40086
unknown <teci_40@hotmail.com>
2018-04-25 00:52:16 -0500

    busqueda clima

--- index.php
+++ index.php
@@ -4,12 +4,23 @@ require 'vendor/autoload.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use GuzzleHttp\Client;
+
+$api_key = getenv('CLAVE_OPEN');
+
 
 $app = new \Slim\App;
 
-$app->get('/clima', function (Request $request, Response $response, array $args){
-	$data = array('nombre' => 'Rodrigo', 'Matricula' => 416054598);
-	return $response->withJson($data);
+$app->get('/clima/{id}', function(Request $request, Response $response, array $agrs) use($app, $api_key){
+	$client = new Client();
+
+	$id = $agrs['id'];
+
+	$url = "http://api.openweathermap.org/data/2.5/weather?id=".$id."&APPID=".$api_key."&units=metric";
+
+	$response = $client->get($url);
+
+	return $response;
 });
 
 $app->run();
